97da7aacd9dfd5e9eac1e56cb3afbc306318a74a
pallavi-pannu-oc <67274173+pallavi-pannu-oc@users.noreply.github.com>
2021-06-17 12:49:36 +0530

    Create train.py

---
+++ train.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+global_batch_size = 2
+# Passing the devices is optional.
+strategy = tf.distribute.MirroredStrategy(devices=["GPU:0", "GPU:1"])
+# Create a dataset
+dataset = tf.data.Dataset.range(4).batch(global_batch_size)
+# Distribute that dataset
+dist_dataset = strategy.experimental_distribute_dataset(dataset)
+@tf.function
+def replica_fn(input):
+  return input*2
+result = []
+# Iterate over the `tf.distribute.DistributedDataset`
+for x in dist_dataset:
+  # process dataset elements
+  result.append(strategy.run(replica_fn, args=(x,)))
+print(result)
